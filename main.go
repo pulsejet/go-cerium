9f6201bd030b41a82c8777c7dedc3fe84f77da3a
e-aakash <09e.aakash@gmail.com>
2020-07-30 15:50:41 +0530

    fix: formatting of comments

--- main.go
+++ main.go
@@ -30,37 +30,37 @@ type spaHandler struct {
 // file located at the index path on the SPA handler will be served. This
 // is suitable behavior for serving an SPA (single page application).
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    // get the absolute path to prevent directory traversal
+	// get the absolute path to prevent directory traversal
 	path, err := filepath.Abs(r.URL.Path)
 	if err != nil {
-        // if we failed to get the absolute path respond with a 400 bad request
-        // and stop
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-    // prepend the path with the path to the static directory
+	// prepend the path with the path to the static directory
 	path = filepath.Join(h.staticPath, path)
 
-    // check whether a file exists at the given path
+	// check whether a file exists at the given path
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
 		// file does not exist, serve index.html
 		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
 		return
 	} else if err != nil {
-        // if we got an error (that wasn't that the file doesn't exist) stating the
-        // file, return a 500 internal server error and stop
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-    // otherwise, use http.FileServer to serve the static dir
+	// otherwise, use http.FileServer to serve the static dir
 	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
 
 func main() {
-        // Load configuration
+	// Load configuration
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -87,7 +87,7 @@ func main() {
 	log.Info("Started server on port ", port)
 	rand.Seed(time.Now().UnixNano())
 
-        // Handle API calls
+	// Handle API calls
 	router.HandleFunc("/api/form", controllers.CreateForm).Methods("POST")
 	router.HandleFunc("/api/forms", controllers.GetAllForms).Methods("GET")
 	router.HandleFunc("/api/form/{id}", controllers.CreateForm).Methods("PUT")
@@ -96,15 +96,15 @@ func main() {
 	router.HandleFunc("/api/response/{formid}", controllers.CreateResponse).Methods("POST")
 	router.HandleFunc("/api/responses/{formid}", controllers.GetResponses).Methods("POST")
 
-        // Handle auth API calls
+	// Handle auth API calls
 	router.HandleFunc("/api/login", controllers.Login).Methods("POST", "GET")
 	router.HandleFunc("/api/logout", controllers.Logout).Methods("GET")
 
-        // Handlse SPA
+	// Handlse SPA
 	spa := spaHandler{staticPath: "dist/cerium", indexPath: "index.html"}
 	router.PathPrefix("/").Handler(spa)
 
-        // Start listening
+	// Start listening
 	err = http.ListenAndServe(":"+port, router)
 	if err != nil {
 		fmt.Print(err)
